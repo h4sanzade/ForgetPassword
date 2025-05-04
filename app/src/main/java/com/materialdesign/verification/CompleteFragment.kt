9271ce13b677e7f252905b582a9f682de8860c75
h4sanzade <h4sanzade@gmail.com>
2025-05-04 05:42:52 +0400

    update

--- app/src/main/java/com/materialdesign/verification/CompleteFragment.kt
+++ app/src/main/java/com/materialdesign/verification/CompleteFragment.kt
@@ -5,55 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.materialdesign.verification.databinding.FragmentCompleteBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CompleteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CompleteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentCompleteBinding
+
+    // User data from SafeArgs
+    private var emailAddress: String = ""
+    private var name: String = ""
+    private var surname: String = ""
+    private var gender: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        // Get all arguments from SafeArgs
+        val args = CompleteFragmentArgs.fromBundle(requireArguments())
+        emailAddress = args.emailAdress
+        name = args.name
+        surname = args.surname
+        gender = args.gender
+        // Note: We also have args.firstPassword, args.repeatPassword, args.verificationCode
+        // but not displaying those for security reasons
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_complete, container, false)
+        binding = FragmentCompleteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CompleteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CompleteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Create a text view to display all user data
+        val userDataTextView = TextView(requireContext()).apply {
+            text = """
+                Registration Complete!
+                
+                Email: $emailAddress
+                Name: $name
+                Surname: $surname
+                Gender: $gender
+            """.trimIndent()
+
+            textSize = 16f
+            setPadding(16, 16, 16, 16)
+        }
+
+        // Add the text view to the container
+        binding.resultContainer.removeAllViews()
+        binding.resultContainer.addView(userDataTextView)
+
+        // Setup save button action
+        binding.saveButton.setOnClickListener {
+            // Here you would typically save the data to a database or shared preferences
+            // For now, just show a message and go back to the start
+            findNavController().navigate(R.id.registrationFragment)
+        }
     }
 }
--- app/src/main/java/com/materialdesign/verification/EmailVerificationFragment.kt
+++ app/src/main/java/com/materialdesign/verification/EmailVerificationFragment.kt
@@ -5,26 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.materialdesign.verification.databinding.FragmentEmailVerificationBinding
-
 
 class EmailVerificationFragment : Fragment() {
     private lateinit var binding: FragmentEmailVerificationBinding
+    private var verificationCode: String = ""
+    private var emailAddress: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        binding = FragmentEmailVerificationBinding.inflate(inflater,container,false)
+        binding = FragmentEmailVerificationBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Get arguments from SafeArgs
         val args = EmailVerificationFragmentArgs.fromBundle(requireArguments())
-        val email = args.emailAdress
+        verificationCode = args.verificationCode
+        emailAddress = args.emailAdress
 
+        // Set up continue button
+        binding.continueButton.setOnClickListener {
+            val enteredCode = binding.emailInputEditText.text.toString()
+
+            // Validate the verification code
+            if (enteredCode == verificationCode) {
+                // Navigate to Create New Password fragment
+                findNavController().navigate(
+                    EmailVerificationFragmentDirections.actionEmailVerificationFragmentToCreateNewPassword(
+                        emailAdress = emailAddress,
+                        verificationCode = verificationCode
+
+                    )
+                )
+            } else {
+                // Show error if code doesn't match
+                Toast.makeText(context, "Invalid verification code", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-
 }
